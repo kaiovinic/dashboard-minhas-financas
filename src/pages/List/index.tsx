479649bc1f63ef4e49575f364696738b1e9319d5
kaiovinic <tidesenvolvimento9@amvox.com.br>
2024-12-20 02:17:19 -0300

    Criando o projeto

---
+++ src/pages/List/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const List: React.FC = (props: Props) => {
+  return <div>List</div>;
+};
+
+export default List;
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const SignIn: React.FC = (props: Props) => {
+  return <div>SignIn</div>;
+};
+
+export default SignIn;
